817aa40cbe1c6042aa60db07387d09b0ebe97eec
dmassiani <davidmassianirennes@gmail.com>
2014-12-12 17:37:03 +0100

    compile first

--- www/app.js
+++ www/app.js
@@ -1,7 +1,21 @@
-var $;
-
-$ = require('jquery');
-
-$(function() {
-  return $('.container').append('Hello World');
+$(document).ready(function() {
+  var menu, menuToggle, signUp;
+  menu = $("#navigation-menu");
+  menuToggle = $("#js-mobile-menu");
+  signUp = $(".sign-up");
+  $(menuToggle).on("click", function(e) {
+    e.preventDefault();
+    menu.slideToggle(function() {
+      if (menu.is(":hidden")) {
+        menu.removeAttr("style");
+      }
+    });
+  });
+  $(".nav .nav-link").click(function() {
+    $(".nav .nav-link").each(function() {
+      $(this).removeClass("active-nav-item");
+    });
+    $(this).addClass("active-nav-item");
+    $(".nav .more").removeClass("active-nav-item");
+  });
 });
