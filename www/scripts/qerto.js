439c4b2abadb132c1990647ff41972cbfc773fd9
dmassiani <davidmassianirennes@gmail.com>
2014-12-18 18:15:33 +0100

    update

--- www/scripts/qerto.js
+++ www/scripts/qerto.js
@@ -126,15 +126,17 @@ $(function() {
                 'msg'           : $('textarea[name=message]').val()
             };
 
-            // $.each(files, function(key, value)
-            // {
-            //     post_data.append(key, value);
-            // });
 
             // console.log(post_data);
 
             var formData = new FormData( $( this )[0] );
+            
+            files = event.target.files;
 
+            $.each(files, function(key, value)
+            {
+                post_data.append(key, value);
+            });
 
             console.log(formData);
 
