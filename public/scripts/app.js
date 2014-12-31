cdff3ff4f600690c6305c9bea3dbced9ddae14ae
dmassiani <davidmassianirennes@gmail.com>
2014-12-31 18:21:55 +0100

    upadte heros

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -12,7 +12,42 @@ require(['enquire', 'snap', 'velocity', 'velocityUI'], function(enquire, snap, v
 
 		var Screen;
 
-		enquire.register("screen and (min-width: 45em)", {
+		$(".js-vertical-tab-content").hide();
+		$(".js-vertical-tab-content:first").show();
+
+		/* if in tab mode */
+
+		$(".js-vertical-tab").click(function(event) {
+		  event.preventDefault();
+
+		  $(".js-vertical-tab-content").hide();
+		  var activeTab = $(this).attr("rel");
+		  $("#"+activeTab).show();
+
+		  $(".js-vertical-tab").removeClass("is-active");
+		  $(this).addClass("is-active");
+
+		  $(".js-vertical-tab-accordion-heading").removeClass("is-active");
+		  $(".js-vertical-tab-accordion-heading[rel^='"+activeTab+"']").addClass("is-active");
+		});
+
+		/* if in accordion mode */
+
+		$(".js-vertical-tab-accordion-heading").click(function(event) {
+		  event.preventDefault();
+
+		  $(".js-vertical-tab-content").hide();
+		  var accordion_activeTab = $(this).attr("rel");
+		  $("#"+accordion_activeTab).show();
+
+		  $(".js-vertical-tab-accordion-heading").removeClass("is-active");
+		  $(this).addClass("is-active");
+
+		  $(".js-vertical-tab").removeClass("is-active");
+		  $(".js-vertical-tab[rel^='"+accordion_activeTab+"']").addClass("is-active");
+		});
+
+		enquire.register("screen and (min-width: 100em)", {
 
 			deferSetup : true,
 			setup : function() {
